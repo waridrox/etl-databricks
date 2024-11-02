4f831a07d65e2d2931ad958dc52618453c87a16d
waridrox <mohdwarid4@gmail.com>
2024-11-02 16:21:44 +0530

    Added first workflow task

--- transform.py
+++ transform.py
@@ -1,4 +1,5 @@
 from pyspark.sql.window import Window
+from pyspark.sql.functions import lead, col, broadcast, collect_set, size, array_contains
 
 
 class Transformer:
@@ -7,3 +8,50 @@ class Transformer:
 
     def transform(self, inputDFs):
         pass
+
+class AirpodsAfterIphoneTransformer(Transformer):
+
+    def transform(self, inputDFs):
+        """
+        Customers who have bought Airpods after buying the iPhone
+        """
+
+        transcatioInputDF = inputDFs.get("transcatioInputDF")
+
+        print("transcatioInputDF in transform")
+
+        transcatioInputDF.show()
+
+        windowSpec = Window.partitionBy("customer_id").orderBy("transaction_date")
+
+        transformedDF = transcatioInputDF.withColumn(
+            "next_product_name", lead("product_name").over(windowSpec)
+        )
+
+        print("Airpods after buying iphone")
+        transformedDF.orderBy("customer_id", "transaction_date", "product_name").show()
+
+        filteredDF = transformedDF.filter(
+            (col("product_name") == "iPhone") & (col("next_product_name") == "AirPods")
+        )
+
+        filteredDF.orderBy("customer_id", "transaction_date", "product_name").show()
+
+        customerInputDF = inputDFs.get("customerInputDF")
+
+        customerInputDF.show()
+
+        joinDF =  customerInputDF.join(
+           broadcast(filteredDF),
+            "customer_id"
+        )
+
+        print("JOINED DF")
+        joinDF.show()
+
+        return joinDF.select(
+            "customer_id",
+            "customer_name",
+            "location"
+        )
+
