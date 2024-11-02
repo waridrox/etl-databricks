4a3b50ffd490487feb56e9a27d4002efe258e2cc
waridrox <mohdwarid4@gmail.com>
2024-11-02 15:12:38 +0530

    add data to dbfs deltaTable

--- loader_factory.py
+++ loader_factory.py
@@ -17,3 +17,32 @@ class DataSink:
 
         raise ValueError("Not Implemented")
 
+class LoadToDBFS(DataSink):
+
+    def load_data_frame(self):
+        
+        self.df.write.mode(self.method).save(self.path)
+
+class LoadToDBFSWithPartition(DataSink):
+
+    def load_data_frame(self):
+        
+        partitionByColumns = self.params.get("partitionByColumns")
+        self.df.write.mode(self.method).partitionBy(*partitionByColumns).save(self.path)
+
+class LoadToDeltaTable(DataSink):
+
+    def load_data_frame(self):
+        
+        
+        self.df.write.format("delta").mode(self.method).saveAsTable(self.path)
+
+def get_sink_source(sink_type, df, path, method, params = None):
+    if sink_type == "dbfs":
+        return LoadToDBFS(df, path, method, params)
+    elif sink_type == "dbfs_with_partition":
+        return LoadToDBFSWithPartition(df, path, method, params)
+    elif sink_type == "delta":
+        return LoadToDeltaTable(df, path, method, params)
+    else:
+        return ValueError(f"Not implemented for sink type: {sink_type}")
