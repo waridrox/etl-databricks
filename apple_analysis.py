3896d6647549a984c2f2e4c6f8e752a8662c554f
waridrox <mohdwarid4@gmail.com>
2024-11-03 10:32:12 +0530

    Added second worflow extract pipeline

--- apple_analysis.py
+++ apple_analysis.py
@@ -22,6 +22,26 @@ class FirstWorkFlow:
         AirPodsAfterIphoneLoader(firstTransformedDF).sink()
 
 
+class SecondWorkFlow:
+    """
+    ETL pipeline to generate the data for all customers who have bought only iPhone and Customers
+    """ 
+    def __init__(self):
+        pass
+
+    def runner(self):
+
+        # Step 1: Extract all required data from different source
+        inputDFs = AirpodsAfterIphoneExtractor().extract()
+
+        # Step 2: Implement the Transformation logic
+        # Customers who have bought Airpods after buying the iPhone
+        onlyAirpodsAndIphoneDF = OnlyAirpodsAndIphone().transform(inputDFs)
+
+        # Step 3: Load all required data to differnt sink
+        OnlyAirpodsAndIPhoneLoader(onlyAirpodsAndIphoneDF).sink()
+
+
 class WorkFlowRunner:
 
     def __init__(self, name):
